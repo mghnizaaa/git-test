defdb178c6c7bb1a3ea4a387d56977866d039dfe
zzq <zzq@java.com>
2021-12-05 17:06:40 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @Author: zzq
+ * @Date: 2021/12/5 17:02
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main (String[] args) {
+        System.out.println("hello git!");
+    }
+}
